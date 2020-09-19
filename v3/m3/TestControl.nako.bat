3fb41725f74753aba9d8c32a35ff6f4e9ef75812
nn4343 <gea01441@nifty.com>
2020-09-19 23:31:40 +0900

    no message

--- v3/m3/TestControl.nako.bat
+++
@@ -1,5 +0,0 @@
-@ ECHO OFF
-REM --- NADESIKO V3 ---
-"C:\nadesiko_3\nadesiko3win32-3.1.2\nodejs\node.exe" "C:\nadesiko_3\nadesiko3win32-3.1.2\src\cnako3.js" "G:\statego\samples\psgg-nadesiko-sample\v3\m3\TestControl.nako.bak"
-ECHO ---
-pause
